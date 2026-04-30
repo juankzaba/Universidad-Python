29a829b7539dad5750470dfbdd1e01c2db416ec2
juankzaba <juankzaba34@gmail.com>
2026-04-30 18:04:48 -0500

    Se agrega el proyecto

---
+++ variables.py
@@ -0,0 +1,14 @@
+#PROGRAMA INFORMACIÓN PERSONAL
+
+#GUARDAR LOS DATOS EN VARIABLES
+nombre = "Salome"
+edad = 14
+pais = "Colombia"
+frase_favorita = "Nunca dejes de soñar"
+
+#MOSTRAR LOS DATOS EN CONSOLA
+
+print('Nombre:', nombre)
+print('edad:', edad)
+print('País:', pais)
+print('Frase Favorita:', frase_favorita)
